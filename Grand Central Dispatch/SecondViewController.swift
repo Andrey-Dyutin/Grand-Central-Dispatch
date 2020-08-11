dfe25839b9b862c3a218907bdd5fafba19520e17
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-11 13:42:11 +0400

    + delay loginAvert

--- Grand Central Dispatch/SecondViewController.swift
+++ Grand Central Dispatch/SecondViewController.swift
@@ -29,6 +29,39 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.loginAvert()
+        }
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
+    
+    fileprivate func loginAvert() {
+        let ac = UIAlertController(title: "Зарегистрированы?",
+                                   message: "Введите ваш логи и пароль",
+                                   preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default,
+                                     handler: nil)
+        let cancelAction = UIAlertAction(title: "Отмена",
+                                         style: .default,
+                                         handler: nil)
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField { (userNameTF) in
+            userNameTF.placeholder = "Введите логин"
+        }
+        ac.addTextField { (userPasswordTF) in
+            userPasswordTF.placeholder = "Введите пароль"
+            userPasswordTF.isSecureTextEntry = true
+        }
+        self.present(ac, animated: true, completion: nil)
     }
     
     fileprivate func fetchImage() {
@@ -46,3 +79,4 @@ class SecondViewController: UIViewController {
     }
     
 }
+
