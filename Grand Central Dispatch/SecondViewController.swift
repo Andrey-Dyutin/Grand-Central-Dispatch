b76062379108a230065bc3148765efa7d164427d
Andrey-Dyutin <55854890+Andrey-Dyutin@users.noreply.github.com>
2020-08-11 12:09:05 +0400

    GCD

---
+++ Grand Central Dispatch/SecondViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SecondViewController.swift
+//  Grand Central Dispatch
+//
+//  Created by Андрей Дютин on 11.08.2020.
+//  Copyright © 2020 Андрей Дютин. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(
+            string: "https://sun9-19.userapi.com/c6UbimIibpndyCz11cO7wJNb0Utmd9WzeMwv2A/riDemsiKD1I.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL,let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+}
